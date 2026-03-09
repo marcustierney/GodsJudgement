df08d196c6009387f1040cd6b6a2c9b0261390c8
Marcus <mstierne@ucsc.edu>
2026-03-08 17:48:49 -0700

    Lose and Win button UI changes

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -11,25 +11,18 @@ public class GameOver : MonoBehaviour
     public TextMeshProUGUI stylePercent;
     public TextMeshProUGUI consumptionPercent;
     public TextMeshProUGUI wavesReachedText;
-    public UnityEngine.UI.Image moralityBar;
-    public UnityEngine.UI.Image styleBar;
-    public UnityEngine.UI.Image consumptionBar;
     public AudioSource audioSource;
     public AudioClip buttonClick;
 
     void Start()
     {
         GameOverData d = GameOverData.Instance;
-        moralityLabel.text = $"Morality ({d.morality}):";
-        styleLabel.text = $"Style ({d.style}):";
-        consumptionLabel.text = $"Consumption ({d.consumption}):";
+        moralityLabel.text = $"Morality ({d.morality})";
+        styleLabel.text = $"Style ({d.style})";
+        consumptionLabel.text = $"Consumption ({d.consumption})";
         moralityPercent.text = $"{d.moralitySatisfaction * 100f:F0}%";
         stylePercent.text = $"{d.styleSatisfaction * 100f:F0}%";
         consumptionPercent.text = $"{d.consumptionSatisfaction * 100f:F0}%";
-        moralityBar.fillAmount = d.moralitySatisfaction;
-        styleBar.fillAmount = d.styleSatisfaction;
-        consumptionBar.fillAmount = d.consumptionSatisfaction;
-
         wavesReachedText.text = $"Waves Survived: {d.wavesReached}";
     }
 
